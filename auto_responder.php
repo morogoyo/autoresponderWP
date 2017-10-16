d183c729850819b05cbd27ac7f8fb360ccb60dbb
Rene Torres (morogoyo) <morogoyo@gmail.com>
2017-10-16 16:49:20 -0400

    added logic to sort thru database and select email

--- auto_responder.php
+++ auto_responder.php
@@ -4,17 +4,14 @@
 /**
 * 
 */
-class AutoResponder {
-	
-	function __construct(argument)
-	{
-		# code...
-	}
-}
+
+
+include_once($_SERVER['DOCUMENT_ROOT'].'/wp-config.php' );
+	require '../../../vendor/autoload.php';
+
 //testing purposes wamp server 
-include_once($_SERVER['DOCUMENT_ROOT'].'/jmmdistributor/wp-config.php' );
-//real credentials
-// include_once($_SERVER['DOCUMENT_ROOT'].'/wp-config.php' );
+//include_once($_SERVER['DOCUMENT_ROOT'].'/jmmdistributor/wp-config.php' );
+
 global $wpdb;
 
  // function select(){
@@ -23,21 +20,49 @@ global $wpdb;
  
  if ($result){ 	
  	foreach ($result as $results) {//looping thru results
-	 	$time = $wpdb->get_var( "SELECT DATEDIFF('$results->date','$date')");//getting comparison from database
-
+	 	$time = $wpdb->get_var( "SELECT DATEDIFF('$date','$results->date')");//getting comparison from database
+	 	var_dump($time);
 	 	// Need to implement timing intervals here or compare to database
-	 	if ($time < 1){
-	 	echo $results->email;
-	 	echo "</br>";
+	 	if ($time == 5 ){
+	 		$day = 5;
+		 	echo " day 5";
+		 	echo "</br>";
+	 	}elseif ($time == 4 ) {
+		 	$day = 4;
+		 	echo "day 4";
+		 	echo "</br>";
+	 	}elseif ($time == 3 ) {
+		 	$day = 3;
+		 	echo "day 3";
+		 	echo "</br>";
+	 	}elseif ($time == 2 ) {
+		 	$day = 2;
+		 	echo "day 2";
+		 	echo "</br>";
+	 	}elseif ($time == 1 ) {
+		 	$day = 1;
+		 	echo "day 1";
+		 	echo "</br>";
+	 	}else {
+	 		$day = 5;
+	 		echo "campaign is done";
 	 	}
  	
- 
- 
-
-
-}
+ }
 }
 
 
+
+$email_table = $wpdb->get_results( "SELECT * FROM ".$wpdb->prefix."email_templates where days = {$day} ");//returns an array
+		
+		echo $email_table->email_subject;
+		foreach ($email_table as $var) {
+			echo "</br>";
+			//echo $var->id;
+			echo "</br>";
+			echo $var->email_subject;
+			echo $var->email;
+
+		}
 
 ?>
